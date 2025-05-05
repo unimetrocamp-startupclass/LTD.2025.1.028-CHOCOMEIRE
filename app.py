e8949d911fd0a667a040569bf78bd48d87135036
joaopaulo-99 <jpsilva.cg@gmail.com>
2025-05-05 22:42:22 +0000

    update

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
-from flask import Flask, render_template # type: ignore
+from flask import Flask, render_template  # type: ignore
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route('/produtos')
+def produtos():
+    return render_template('produtos.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/cadastro')
+def cadastro():
+    return render_template('cadastro.html')
 
 if __name__ == '__main__':
     app.run(debug=True)

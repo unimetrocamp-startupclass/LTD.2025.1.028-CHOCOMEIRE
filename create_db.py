adb05ead4e7cb36cb80c18c849ab7a9d556d4d05
joaopaulo-99 <jpsilva.cg@gmail.com>
2025-06-17 18:16:14 +0000

    update

--- create_db.py
+++ create_db.py
@@ -1,5 +1,8 @@
+import os
 from app import db, app
+
+# Garante que a pasta "instance/" existe
+os.makedirs(os.path.join(app.root_path, 'instance'), exist_ok=True)
 
 with app.app_context():
     db.create_all()
-    print("Banco de dados criado com sucesso!")
